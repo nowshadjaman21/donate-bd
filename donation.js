710093d62ed9f0c6adbf6d8ef522ac525f952a61
nowshadjaman21 <nowshad.nirob@gmail.com>
2024-09-23 15:07:44 +0600

    history added

--- donation.js
+++ donation.js
@@ -1,13 +1,27 @@
 document.getElementById('quota-donate-btn').addEventListener('click', function () {
   showDonations('quota-donate-input', 'quota-total-donation');
+  const addMoneyNumber = getValuebyId('quota-donate-input');
+  const h2 = document.createElement('h2');
+  h2.innerText = `${addMoneyNumber} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
+  document.getElementById('history-container').appendChild(h2);
+  
+  
 })  ;
 
 document.getElementById('feni-donate-btn').addEventListener('click', function () {
   showDonations('feni-donate-input', 'feni-total-donation');
+  const addMoneyNumber = getValuebyId('quota-donate-input');
+  const h2 = document.createElement('h2');
+  h2.innerText = `${addMoneyNumber} Taka is Donated for Flood relif in Feni, Bangladesh`;
+  document.getElementById('history-container').appendChild(h2);
 })  ;
 
 document.getElementById('noakhali-donate-btn').addEventListener('click', function () {
   showDonations('noakhali-donate-input', 'noakhali-total-donation');
+  const addMoneyNumber = getValuebyId('quota-donate-input');
+  const h2 = document.createElement('h2');
+  h2.innerText = `${addMoneyNumber} Taka is Donated for Flood relif in Noakhali, Bangladesh`;
+  document.getElementById('history-container').appendChild(h2);
 })  ;
 
 
--- utilities.js
+++ utilities.js
@@ -7,11 +7,23 @@ function showDonations(input,total) {
  
     const addMoney =getValuebyId(input);
     const addMoneyNumber = parseFloat(addMoney);
+    
     if (!isNaN(addMoneyNumber) && addMoneyNumber > 0) {
       const donatedAmount = document.getElementById(total).innerText;
       const donatedMoney = parseFloat(donatedAmount);
       const totalDonation = donatedMoney + addMoneyNumber;
   document.getElementById(total).innerText = totalDonation;
+  const balance = document.getElementById('balance').innerText;
+ 
+  const balanceNumber = parseFloat(balance);
+  if (balanceNumber >= addMoneyNumber && addMoneyNumber > 0) {
+    const totalBalance = balanceNumber - addMoneyNumber;
+    document.getElementById('balance').innerText = totalBalance;
+  }
+  else{
+    alert('You don\'t have enough balance');
+  
+    };
     }
     else{
       alert('Please enter a valid amount');
